85ed351f58ed963591119a75cc7626f614c73768
Nakshatra Joshi <23cd3025@rgipt.ac.in>
2024-05-15 14:27:42 +0530

    Add files via upload

---
+++ run_requirements.bat
@@ -0,0 +1,15 @@
+@echo off
+echo Installing Flask...
+pip install Flask
+
+echo Installing pandas...
+pip install pandas
+
+echo Installing scikit-learn...
+pip install scikit-learn
+
+echo Installing Flask-Cors...
+pip install flask-cors
+
+echo All packages installed successfully.
+pause
